12f1e9f8478938148358068622afb4b1d51ddad9
SagarBarua1010 <sagarbarua1010@gmail.com>
2024-09-16 16:12:28 +0600

    route

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import Home from "./componets/Home";
 import Login from "./componets/Login";
 import Register from "./componets/Register";
 import AuthProviders from "./providers/AuthProviders";
+import Orders from "./componets/Orders";
+import PrivateRoute from "./routes/PrivateRoute";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +27,10 @@ const router = createBrowserRouter([
         path: "/register",
         element: <Register></Register>,
       },
+      {
+        path:'/orders',
+        element:<PrivateRoute><Orders></Orders></PrivateRoute>
+      }
     ],
   },
 ]);
